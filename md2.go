31577d4bb189c77e4dfe7d37026f482b83082a60
WinRAR <htruong@tnhh.net>
2013-02-27 18:38:40 -0600

    Update md2.go

--- md2.go
+++ md2.go
@@ -1,14 +1,13 @@
-// Copyright 2009 The Go Authors. All rights reserved.
+// Copyright 2012-2013 Huan Truong. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package md5 implements the MD2 hash algorithm as defined in RFC 1321.
+// Package md2 implements the MD2 hash algorithm as defined in RFC 1319.
 package md2
 
 import (
 	//"crypto"
 	"hash"
-	//"fmt"
 )
 
 func init() {
